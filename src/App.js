c35a8cbd890c0fd1212823b331a65fec61291b88
Maria Alarcon <mariagaa993@gmail.com>
2020-12-11 22:41:12 -0400

    estructura del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.scss';
+import Routes from './routes/Routes';
+
+const App = () => {
+  	return (
+		<Fragment>
+    		<Routes />
+		</Fragment>
+  	);
+}
+
+export default App;
---
+++ src/routes/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+  	return (
+		<Switch>
+    		
+		</Switch>
+  	);
+}
+
+export default Routes;
